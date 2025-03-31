dbf9a1dfff669c26b7981095ba2ed71c7bde8d10
Sergio Lillo <sergiolillom@gmail.com>
2025-03-31 19:39:45 +0200

    hero-page with computed signals, some styles fixes

---
+++ src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,35 @@
+import { Component, computed, signal } from "@angular/core";
+
+
+@Component({
+    templateUrl: './hero-page.component.html',
+    //styleUrl: './hero-page.component.css'
+})
+export class HeroPageComponent {
+
+    name = signal('Ironman');
+    age = signal(45);
+
+    nameUpperCase = computed(() => {
+       return this.name().toUpperCase();
+      });
+
+    heroDescription = computed(() => {
+        return `${this.name()} - ${this.age()}`
+    });
+
+    changeHero() {
+        this.name.update(current => 'Spiderman');
+        this.age.update(currente => 22);
+    }
+
+    resetForm() {
+        this.name.update(current => 'Ironman');
+        this.age.update(current => 45); 
+    }
+
+    changeAge() {
+        this.age.update(current => 60);
+    }
+
+}
